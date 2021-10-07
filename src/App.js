09dedcc36104e08526584963e8d8f2acfe517a5d
Zamal Hossain <thezamalian@gmail.com>
2021-10-07 22:33:50 +0600

    order review & routing have been set up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import Inventory from './components/Inventory/Inventory';
 import Shop from './components/Shop/Shop';
+import OrderReview from './components/OrderReview/OrderReview';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
+
   return (
     <div>
-      <Header></Header>
-      <Shop></Shop>
+      <Router >
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Shop></Shop>
+          </Route>
+          <Route path="/shop">
+            <Shop></Shop>
+          </Route>
+          <Route path="/inventory">
+            <Inventory></Inventory>
+          </Route>
+          <Route path="/review">
+            <OrderReview></OrderReview>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Inventory = () => {
+    useEffect(() => {
+        fetch('/https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(data => console.log(data))
+    });
+
     return (
         <div>
             <h2>This is inventory</h2>
